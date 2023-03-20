8aad0a1257db9fc721f463b813b94089e6a81ae9
Yvnn16 <yvonneweru5@gmail.com>
2023-03-20 16:12:56 +0000

    Tasks

--- models/state.py
+++ models/state.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import models
+""" State Module for HBNB project """
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-
-    Attributes:
-        name: input name
-
-    """
-
-    __tablename__ = "states"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
     else:
-        name = ''
-
         @property
         def cities(self):
-            """Returns the list of `City` instances
-            with `state_id` equals to the current
-            """
-
-            cities = list()
-
-            for _id, city in models.storage.all(City).items():
-                if city.state_id == self.id:
-                    cities.append(city)
-
-            return cities
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
